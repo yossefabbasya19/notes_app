0bc18d7a110aa6eba38c5218673fd7b60685ac4a
Nona <yossefabasya19@gmail.com>
2025-04-13 20:56:58 +0200

    create feature app bar

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.notes_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.0.13004108"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
